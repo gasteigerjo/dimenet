13e814606aebeafbb7542a4ed01ed31978ddda4e
Johannes Klicpera <klicpera@in.tum.de>
2020-02-27 16:51:50 +0100

    Exclude order n in bessel_basis

--- dimenet/model/layers/basis_utils.py
+++ dimenet/model/layers/basis_utils.py
@@ -13,15 +13,15 @@ def Jn(r, n):
 
 def Jn_zeros(n, k):
     """
-    Compute the first k zeros of the spherical bessel functions up to order n (included)
+    Compute the first k zeros of the spherical bessel functions up to order n (excluded)
     """
-    zerosj = np.zeros((n+1, k), dtype="float32")
-    zerosj[0] = np.arange(1, k+1) * np.pi
-    points = np.arange(1, k+n+1) * np.pi
-    racines = np.zeros(k+n, dtype="float32")
-    for i in range(1, n+1):
-        for j in range(k+n-i):
-            foo = brentq(Jn, points[j], points[j+1], (i,))
+    zerosj = np.zeros((n, k), dtype="float32")
+    zerosj[0] = np.arange(1, k + 1) * np.pi
+    points = np.arange(1, k + n) * np.pi
+    racines = np.zeros(k + n - 1, dtype="float32")
+    for i in range(1, n):
+        for j in range(k + n - 1 - i):
+            foo = brentq(Jn, points[j], points[j + 1], (i,))
             racines[j] = foo
         points = racines
         zerosj[i][:k] = racines[:k]
@@ -31,15 +31,15 @@ def Jn_zeros(n, k):
 
 def spherical_bessel_formulas(n):
     """
-    Computes the sympy formulas for the spherical bessel functions up to order n (included)
+    Computes the sympy formulas for the spherical bessel functions up to order n (excluded)
     """
     x = sym.symbols('x')
 
     f = [sym.sin(x)/x]
     a = sym.sin(x)/x
-    for i in range(n):
+    for i in range(1, n):
         b = sym.diff(a, x)/x
-        f += [sym.simplify(b*(-x)**(i+1))]
+        f += [sym.simplify(b*(-x)**i)]
         a = sym.simplify(b)
     return f
 
@@ -47,12 +47,12 @@ def spherical_bessel_formulas(n):
 def bessel_basis(n, k):
     """
     Compute the sympy formulas for the normalized and rescaled spherical bessel functions up to
-    order n (included) and maximum frequency k (excluded).
+    order n (excluded) and maximum frequency k (excluded).
     """
 
     zeros = Jn_zeros(n, k)
     normalizer = []
-    for order in range(n+1):
+    for order in range(n):
         normalizer_tmp = []
         for i in range(k):
             normalizer_tmp += [0.5*Jn(zeros[order, i], order+1)**2]
@@ -62,7 +62,7 @@ def bessel_basis(n, k):
     f = spherical_bessel_formulas(n)
     x = sym.symbols('x')
     bess_basis = []
-    for order in range(n + 1):
+    for order in range(n):
         bess_basis_tmp = []
         for i in range(k):
             bess_basis_tmp += [sym.simplify(normalizer[order]
@@ -83,40 +83,39 @@ def sph_harm_prefactor(l, m):
 
 def associated_legendre_polynomials(l, zero_m_only=True):
     """
-    Computes sympy formulas of the associated legendre polynomials up to order l
+    Computes sympy formulas of the associated legendre polynomials up to order l (excluded).
     """
     z = sym.symbols('z')
-    P_l_m = [[0]*(j+1) for j in range(l + 1)]
+    P_l_m = [[0]*(j+1) for j in range(l)]
 
     P_l_m[0][0] = 1
     if l > 0:
         P_l_m[1][0] = z
 
-        for j in range(2, l+1):
+        for j in range(2, l):
             P_l_m[j][0] = sym.simplify(
                 ((2*j-1)*z*P_l_m[j-1][0] - (j-1)*P_l_m[j-2][0])/j)
         if not zero_m_only:
             for i in range(1, l):
                 P_l_m[i][i] = sym.simplify((1-2*i)*P_l_m[i-1][i-1])
-                P_l_m[i+1][i] = sym.simplify((2*i+1)*z*P_l_m[i][i])
-                for j in range(i+2, l+1):
+                if i + 1 < l:
+                    P_l_m[i+1][i] = sym.simplify((2*i+1)*z*P_l_m[i][i])
+                for j in range(i + 2, l):
                     P_l_m[j][i] = sym.simplify(
-                        ((2*j-1)*z*P_l_m[j-1][i] - (i+j-1)*P_l_m[j-2][i])/(j-i))
-
-            P_l_m[l][l] = sym.simplify((1-2*l)*P_l_m[l-1][l-1])
+                        ((2*j-1) * z * P_l_m[j-1][i] - (i+j-1) * P_l_m[j-2][i]) / (j - i))
 
     return P_l_m
 
 
 def real_sph_harm(l, zero_m_only=True, spherical_coordinates=True):
     """
-    Computes formula strings of the the real part of the spherical harmoncis up to order l.
+    Computes formula strings of the the real part of the spherical harmonics up to order l (excluded).
     Variables are either cartesian coordinates x,y,z on the unit sphere or spherical coordinates phi and theta.
     """
     if not zero_m_only:
         S_m = [0]
         C_m = [1]
-        for i in range(1, l+1):
+        for i in range(1, l):
             x = sym.symbols('x')
             y = sym.symbols('y')
             S_m += [x*S_m[i-1] + y*C_m[i-1]]
@@ -139,17 +138,17 @@ def real_sph_harm(l, zero_m_only=True, spherical_coordinates=True):
                 C_m[i] = C_m[i].subs(x, sym.sin(
                     theta)*sym.cos(phi)).subs(y, sym.sin(theta)*sym.sin(phi))
 
-    Y_func_l_m = [['0']*(2*j+1) for j in range(l+1)]
-    for i in range(l+1):
+    Y_func_l_m = [['0']*(2*j + 1) for j in range(l)]
+    for i in range(l):
         Y_func_l_m[i][0] = sym.simplify(sph_harm_prefactor(i, 0) * P_l_m[i][0])
 
     if not zero_m_only:
-        for i in range(1, l+1):
-            for j in range(1, i+1):
+        for i in range(1, l):
+            for j in range(1, i + 1):
                 Y_func_l_m[i][j] = sym.simplify(
                     2**0.5 * sph_harm_prefactor(i, j) * C_m[j] * P_l_m[i][j])
-        for i in range(1, l+1):
-            for j in range(1, i+1):
+        for i in range(1, l):
+            for j in range(1, i + 1):
                 Y_func_l_m[i][-j] = sym.simplify(
                     2**0.5 * sph_harm_prefactor(i, -j) * S_m[j] * P_l_m[i][j])
 
