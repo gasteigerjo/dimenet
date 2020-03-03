5799dbd44c43303e7109251a75978e5c2fc544d2
Johannes Klicpera <klicpera@in.tum.de>
2020-03-03 18:06:33 +0100

    Faster SBF layer

--- dimenet/model/layers/spherical_basis_layer.py
+++ dimenet/model/layers/spherical_basis_layer.py
@@ -21,27 +21,34 @@ class SphericalBasisLayer(layers.Layer):
         # retrieve formulas
         self.bessel_formulas = bessel_basis(num_spherical, num_radial)
         self.sph_harm_formulas = real_sph_harm(num_spherical)
-        self.funcs = []
+        self.sph_funcs = []
+        self.bessel_funcs = []
 
         # convert to tensorflow functions
         x = sym.symbols('x')
         theta = sym.symbols('theta')
         for i in range(num_spherical):
+            if i == 0:
+                first_sph = sym.lambdify([theta], self.sph_harm_formulas[i][0], 'tensorflow')(0)
+                self.sph_funcs.append(lambda tensor: tf.zeros_like(tensor) + first_sph)
+            else:
+                self.sph_funcs.append(sym.lambdify([theta], self.sph_harm_formulas[i][0], 'tensorflow'))
             for j in range(num_radial):
-                self.funcs.append(sym.lambdify(
-                    [x, theta], self.sph_harm_formulas[i][0] * self.bessel_formulas[i][j], 'tensorflow'))
+                self.bessel_funcs.append(sym.lambdify([x], self.bessel_formulas[i][j], 'tensorflow'))
 
     def call(self, inputs):
         d, Angles, id_expand_kj = inputs
 
         d_scaled = d * self.inv_cutoff
-        d_scaled = tf.gather(d_scaled, id_expand_kj)
-
-        rbf = [f(d_scaled, Angles) for f in self.funcs]
-        rbf = tf.transpose(tf.stack(rbf))
-
-        # Necessary for proper broadcasting behaviour
-        d_scaled = tf.expand_dims(d_scaled, -1)
+        rbf = [f(d_scaled) for f in self.bessel_funcs]
+        rbf = tf.stack(rbf, axis=1)
 
         d_cutoff = self.envelope(d_scaled)
-        return d_cutoff * rbf
+        rbf_env = d_cutoff[:, None] * rbf
+        rbf_env = tf.gather(rbf_env, id_expand_kj)
+
+        cbf = [f(Angles) for f in self.sph_funcs]
+        cbf = tf.stack(cbf, axis=1)
+        cbf = tf.repeat(cbf, self.num_radial, axis=1)
+
+        return rbf_env * cbf
