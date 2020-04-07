72f77b5288503f10515878a24e89a2f0a6acc9b1
Johannes Klicpera <klicpera@in.tum.de>
2020-04-07 14:32:38 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
 setup(name='dimenet',
-      version='0.1',
+      version='1.0',
       description='Directional Message Passing for Molecular Graphs',
       author='Johannes Klicpera, Janek Groß, Stephan Günnemann',
       author_email='klicpera@in.tum.de',
       packages=['dimenet'],
+      install_requires=install_requires,
       zip_safe=False)
