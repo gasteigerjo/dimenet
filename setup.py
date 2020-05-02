3849902735d5960defe537016916c98497d7522b
Johannes Klicpera <klicpera@in.tum.de>
2020-05-02 20:40:15 +0200

    Move requirements into setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 
-with open('requirements.txt', 'r') as f:
-    install_requires = f.read().splitlines()
+install_requires = [
+        "numpy",
+        "scipy>=1.3",
+        "sympy>=1.5",
+        "tensorflow>=2.1",
+        "tensorflow_addons",
+        "tqdm",
+]
 
-setup(name='dimenet',
-      version='1.0',
-      description='Directional Message Passing for Molecular Graphs',
-      author='Johannes Klicpera, Janek Groß, Stephan Günnemann',
-      author_email='klicpera@in.tum.de',
-      packages=['dimenet'],
-      install_requires=install_requires,
-      zip_safe=False)
+setup(
+        name='dimenet',
+        version='1.0',
+        description='Directional Message Passing for Molecular Graphs',
+        author='Johannes Klicpera, Janek Groß, Stephan Günnemann',
+        author_email='klicpera@in.tum.de',
+        packages=['dimenet'],
+        install_requires=install_requires,
+        zip_safe=False,
+)
